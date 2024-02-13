16df1c41273498101a5c63d8b8a1f41ebb7c665e
raein1337 <159170188+raein1337@users.noreply.github.com>
2024-02-13 03:48:47 -0500

    Update fxmanifest.lua
    Not anything major lol, whvsky was my old name.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Whvsky'
-description 'Despawn Ped Script For Project X!'
+author 'Raein1337 aka Whvsky'
+description 'Despawn Ped Script made by Raein1337!'
 version '1.0.0'
 
 client_script 'client.lua'
